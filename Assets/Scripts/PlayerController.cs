2ec55a0c105e7378e8b062384f00e18ade093355
Nekot94 <nekomato9@gmail.com>
2017-02-05 19:00:23 +0300

    first day

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = 1.0f;
+
+    private Rigidbody2D _rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        _rigidbody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        _rigidbody.AddForce(movement * speed);
+
+    }
+}

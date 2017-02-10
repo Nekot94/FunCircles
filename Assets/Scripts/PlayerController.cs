13920b8aa785950733546840b7b5f56d40172c22
Nekot94 <nekomato9@gmail.com>
2017-02-11 01:09:33 +0300

    Final circle

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,20 +6,23 @@ using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
     public float speed = 1.0f;
-	public Text winText;
-	public Text scoreText;
-	public int winCount = 5;
-	private int count;
+    public Text winText;
+    public Text scoreText;
+    public int winCount = 40;
+    private int count;
+    private bool isInvisible;
 
     private Rigidbody2D _rigidbody;
 
-	// Use this for initialization
-	void Start () {
-        _rigidbody = GetComponent<Rigidbody2D> ();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+    // Use this for initialization
+    void Start() {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        scoreText.text = "Очки:" + count.ToString() + "/" + winCount.ToString();
+        isInvisible = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
@@ -27,19 +30,48 @@ public class PlayerController : MonoBehaviour {
         _rigidbody.AddForce(movement * speed);
 
     }
-	void OnTriggerEnter2D(Collider2D collision) {
-		if (collision.gameObject.CompareTag("PickUp")) {
-			collision.gameObject.SetActive (false);
-			count += 1;
-			scoreText.text = "Очки:" + count.ToString () + "/" + winCount.ToString ();
-			if (count >= winCount) {
-				winText.text = "Ты победил";
-				StartCoroutine (Restart (3f));
-				}
-		}
-	}
-	private IEnumerator Restart(float time) {
-		yield return new WaitForSeconds (time);
-		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
-	}
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("PickUp")) {
+            collision.gameObject.SetActive(false);
+            count += 1;
+            scoreText.text = "Очки:" + count.ToString() + "/" + winCount.ToString();
+            if (count >= winCount) {
+                winText.text = "Ты победил";
+                StartCoroutine(NextScene(7f));
+                GetComponent<CircleCollider2D>().enabled = false;
+            }
+        }
+
+        if (collision.gameObject.CompareTag("InvPotion")) {
+
+            collision.gameObject.SetActive(false);
+            StartCoroutine(InvisibleTime(10f));
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll) {
+        if (coll.gameObject.tag == "Enemy" && !isInvisible) {
+            winText.text = "ты проиграл";
+            winText.color = Color.red;
+            StartCoroutine(Restart(7f));
+            GetComponent<CircleCollider2D>().enabled = false;
+
+        }
+    }
+    private IEnumerator Restart(float time) {
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private IEnumerator InvisibleTime(float time) {
+        isInvisible = true;
+        transform.localScale += new Vector3(2f, 2f, 0);
+        yield return new WaitForSeconds(time);
+        transform.localScale -= new Vector3(2f, 2f, 0);
+        isInvisible = false;
+    }
+    private IEnumerator NextScene(float time) {
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene("Scene2");
+    }
 }
